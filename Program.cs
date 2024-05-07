6e3463a45a6025c2e156cdff7f00855c9f89f5ca
JWMiller006 <95261008+JWMiller006@users.noreply.github.com>
2024-05-07 17:20:39 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MillerInc.PhysicsEngine.TestingEnv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string? input = Console.ReadLine();
+            input = input ?? "No input was given.";
+            Console.WriteLine(input);
+            Test.TestMethod();
+        }
+    }
+
+    public class Test
+    {
+        public static void TestMethod()
+        {
+            // Create a new instance of the PhysicsObject
+            PhysicsObject obj = new();
+            // Create another instance of the PhysicsObject
+            PhysicsObject obj2 = new();
+
+            // Simulate the collision between obj and obj2
+            //obj.Collide(obj2);
+
+            // Print the results of the collision
+            //Console.WriteLine(obj.CollisionResult);
+        }
+    }
+}
